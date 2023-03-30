58e6b0953b5592b8d57532aea67f99d9e46fe15a
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-30 17:47:44 +0200

    New Post model

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostModel(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'created', 'status')
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUserModel
 
-# Register your models here.
+
+@admin.register(CustomUserModel)
+class CustomUser(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'first_name', 'last_name', 'type')
