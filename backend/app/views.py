0183d1203c82de0a4d8859df27bed3271947a904
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-31 17:46:20 +0200

    Pagination feature added and refactoring of post_list view into a generic ListView

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
-    ctx = {'posts': posts}
-    return render(request, 'app/post/list.html', ctx)
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+#
+# def post_list(request):
+#     object_list = Post.published.all()
+#
+#     paginator = Paginator(object_list, 2)
+#     page = request.GET.get('page')
+#     print(page)
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     ctx = {'posts': posts, 'page': page}
+#
+#     return render(request, 'app/post/list.html', ctx)
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    # model = Post
+    template_name = 'app/post/list.html'
+    context_object_name = 'posts'  # default name = object_list
+    paginate_by = 2
 
 
 def post_detail(request, year, month, day, post):
